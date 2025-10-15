e0af55ec36af6f33a6071b5c5758f8a3fb994fda
HarishKaushik15 <harishkr424@gmail.com>
2025-10-15 15:39:33 -0500

    Additional commit

--- Colon/Colon-training-model/ColonCancerDataset.py
+++ Colon/Colon-training-model/ColonCancerDataset.py
@@ -20,20 +20,50 @@ print('Numpy version', np.__version__)
 print('Pandas version', pd.__version__)
 
 class ColonCancerDataset(Dataset):
-    def __init__(self, csv_file, root_dir, transform=None):
-        self.data_frame = pd.read_csv(csv_file)
-        self.root_dir = root_dir
-        self.transform = transform
+    def __init__(self, data_dir, transform=None):
+        self.data = ImageFolder(data_dir, transform=transform)
 
     def __len__(self):
-        return len(self.data_frame)
+        return len(self.data)
 
     def __getitem__(self, idx):
-        img_name = f"{self.root_dir}/{self.data_frame.iloc[idx, 0]}"
-        image = torchvision.io.read_image(img_name).float() / 255.0  # Normalize to [0, 1]
-        label = self.data_frame.iloc[idx, 1]
+        return self.data[idx]
+    
+    @property
+    def classes(self):
+        return self.data.classes
 
-        if self.transform:
-            image = self.transform(image)
 
-        return image, label
+transform = transforms.Compose([transforms.ToTensor()])
+dataset = ColonCancerDataset(data_dir='./colon_output/train', transform=transform)
+len(dataset)
+target_to_class = {v: k for k, v in ImageFolder('./colon_output/train').class_to_idx.items()}
+print(target_to_class)
+
+dataloader = DataLoader(dataset, batch_size=16, shuffle=True)
+for image, label in dataset:
+    break
+
+for images, labels in dataloader:
+    break
+
+class ColonCancerClassifer(nn.Module):
+    def __init__(self, num_classes=3):
+        super(ColonCancerClassifer, self).__init__()
+        self.base_model = timm.create_model('efficientnet_b0', pretrained=True)
+        self.features = nn.Sequential(*list(self.base_model.children())[:-1])
+
+        enet_out_size = 1280
+        # Make a classifier
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(enet_out_size, num_classes)
+        )
+    
+    def forward(self, x):
+        x = self.features(x)
+        output = self.classifier(x)
+        return output
+
+
+        
