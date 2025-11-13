bd1eb73c359ced51f4faae1e8f363ab8c197f858
Krish-Patel656 <krish.patel.060506@gmail.com>
2025-11-12 19:41:47 -0600

    Create oral_cancer.py

---
+++ backend/models/oral_cancer.py
@@ -0,0 +1,17 @@
+# backend/models/oral_cancer.py
+import torch
+import torch.nn as nn
+import timm
+
+class OralCancerClassifier(nn.Module):
+    def __init__(self, num_classes=2):
+        super(OralCancerClassifier, self).__init__()
+        self.base_model = timm.create_model('efficientnet_b0', pretrained=True)
+        self.features = nn.Sequential(*list(self.base_model.children())[:-1])
+        enet_out_size = 1280
+        self.classifier = nn.Linear(enet_out_size, num_classes)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = torch.flatten(x, 1)
+        return self.classifier(x)
