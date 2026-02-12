89b312653d781e1e5d22f622d6a00c490313a4ab
shmr <sreeharimr2005@gmail.com>
2026-02-11 19:31:56 -0600

    Delete frontend directory

--- frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"zDFHCbhr9KXllnv0KQrqB5V84yZqJNqYd4BpNg/WPPw=\"\n}"
--- frontend/.next/dev/static/chunks/frontend_app_dashboard_page_tsx_e4e6ef7d._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/frontend_74bf3d0d._.js"
-],
-    source: "dynamic"
-});
